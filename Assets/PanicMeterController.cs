588b70a1a8cad2de5fbd2c401be7210dcc223482
Ongun1905 <53625744+Ongun1905@users.noreply.github.com>
2021-10-21 10:58:08 +0200

    PanicMeter

---
+++ Assets/PanicMeterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanicMeterController : MonoBehaviour
+{
+    public Slider panicMeter;
+    private float startValue = 0.0f;
+    public Camera mainCamera;
+    public float lowFov = 40.0f;
+    public float defaultFov = 56.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+
+
+    }
+
+    private void Start()
+    {
+        //panic meter  start value
+        panicMeter.value = startValue;
+    }
+    
+
+    private void Update()
+    {
+      
+        CameraSettings();
+        
+    }
+
+    //change camera fov once panicmeter is really high
+    private void CameraSettings()
+    {
+        if(panicMeter.value > 0.8f)
+        {
+            mainCamera.fieldOfView = lowFov;
+        } else
+        {
+            mainCamera.fieldOfView = 56.0f;
+        }
+    }
+
+}
