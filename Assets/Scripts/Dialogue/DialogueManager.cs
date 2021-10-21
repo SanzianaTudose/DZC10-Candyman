cea4d9d611639a25f27f5590ca76346d09be527c
SanzianaTudose <39738506+SanzianaTudose@users.noreply.github.com>
2021-10-21 10:23:46 +0200

    Candyman Functionality
    TODO:
    -  proper candy UI layout
    - candy layout (implementation depends on Player, but should be really easy to do)

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text nameText;
+    [SerializeField] private TMP_Text dialogueText;
+
+    public void StartDialogue(Dialogue dialogue) {
+        nameText.text = dialogue.name;
+        dialogueText.text = dialogue.sentence;
+        
+        dialogueBox.SetActive(true);
+    }
+
+    public void EndDialogue() {
+        dialogueBox.SetActive(false);
+    }
+
+}
