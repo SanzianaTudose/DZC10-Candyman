26a768db289b083d8800122fe10ccb7c3d5383a8
SanzianaTudose <39738506+SanzianaTudose@users.noreply.github.com>
2021-10-24 17:12:08 +0200

    Custom indicators based on array
    + programmatically instantiate indicators from array
    
    next:
    - instantiate indicators on quest received
    - destroy indicators when object is collected

--- Assets/Scripts/CompassController.cs
+++ Assets/Scripts/CompassController.cs
@@ -4,28 +4,51 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CompassController : MonoBehaviour {
-    Quaternion dirOffset = Quaternion.Euler(0, 0, -90);
-
+    [SerializeField] private List<Transform> targetObjects;
+    [SerializeField] private GameObject objectIndicatorPrefab;
     private Transform player;
-    [SerializeField] private Transform targetObject;
 
-    [SerializeField] private RectTransform objectIndicator;
+    private Transform indicatorsContainer;
+    private List<Transform> indicators;
+
+    private Quaternion dirOffset = Quaternion.Euler(0, 0, -90);
 
     void Start() {
         player = PlayerManager.instance.player.transform;
+        if (player == null)
+            Debug.LogError("CompassController: Player instance not found.");
+
+        indicatorsContainer = transform.GetChild(0);
+        if (player == null)
+            Debug.LogError("CompassController: Indicators child not found.");
+
+        InstantiateIndicators();
     }
 
     void Update() {
-        SetIndicatorDirection();
+        SetIndicatorsDirection();
     }
 
-    private void SetIndicatorDirection() {
-        Vector3 dir = player.position - targetObject.position;
+    private void InstantiateIndicators() {
+        indicators = new List<Transform>();
 
-        Quaternion objectDir = Quaternion.LookRotation(dir);
-        objectDir.z = -objectDir.y;
-        objectDir.x = objectDir.y = 0;
+        foreach (Transform target in targetObjects) {
+            GameObject newIndicator = Instantiate(objectIndicatorPrefab, objectIndicatorPrefab.transform.position, objectIndicatorPrefab.transform.rotation);
+            newIndicator.transform.SetParent(indicatorsContainer, false);
 
-        objectIndicator.localRotation = objectDir * dirOffset;
+            indicators.Add(newIndicator.transform);
+        }
     }
+
+    private void SetIndicatorsDirection() {
+        for (int i = 0; i < targetObjects.Count; i++) { 
+            Vector3 dir = player.position - targetObjects[i].position;
+
+            Quaternion objectDir = Quaternion.LookRotation(dir);
+            objectDir.z = -objectDir.y;
+            objectDir.x = objectDir.y = 0;
+
+            indicators[i].localRotation = objectDir * dirOffset;
+        }
+     }
 }
