7e31ca804b1b975905ea23a976fa0fcd4dd0407c
SanzianaTudose <39738506+SanzianaTudose@users.noreply.github.com>
2021-10-29 21:00:35 +0200

    StartMenu done
    + play story summary
    + scene fade in transitions between MenuScene and SchoolScene

--- Assets/Scripts/Menu/StartMenu.cs
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-   public void OnClickPlay() {
-        SceneManager.LoadScene("SchoolScene");
+    [SerializeField] private GameObject videoPlayerObject;
+    [SerializeField] private GameObject imageObject;
+    [SerializeField] private GameObject sceneChangerObject;
+    private VideoPlayer videoPlayer;
+    private SceneChanger sceneChanger;
+
+
+    private void Start() {
+        videoPlayer = videoPlayerObject.GetComponent<VideoPlayer>();
+        if (videoPlayer == null)
+            Debug.LogError("StartMenu: VideoPlayer component not found on VideoPlayer GameObject.");
+        sceneChanger = sceneChangerObject.GetComponent<SceneChanger>();
+        if (sceneChanger == null)
+            Debug.LogError("StartMenu: SceneChanger component not found on sceneChangerObject");
+    }
+
+    public void OnClickPlay() {
+        imageObject.SetActive(true);
+        videoPlayer.Play();
+
+        StartCoroutine(LoadSceneAfterDelay(25f));
+    }
+
+    IEnumerator LoadSceneAfterDelay(float delay) {
+        yield return new WaitForSeconds(delay);
+        sceneChanger.FadeToScene("SchoolScene");
     }
 }
