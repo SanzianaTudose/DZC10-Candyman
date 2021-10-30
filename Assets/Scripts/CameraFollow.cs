451763dcf38ad4be30c9639fa8b12819d8b32f2d
SanzianaTudose <39738506+SanzianaTudose@users.noreply.github.com>
2021-10-30 13:39:54 +0200

    Menus (#12)
    * Basic StartMenu Layout
    
    TODO:
    - add in StarWars cutscene
    - About Section text
    + End Menu
    
    * StartMenu done
    
    + play story summary
    + scene fade in transitions between MenuScene and SchoolScene
    
    * Simple EndMenu added
    
    + scene change between EndScene and EndMenu scene
    
    * merge done

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,28 +6,7 @@ public class CameraFollow : MonoBehaviour
 	public Transform target;
 	public float smoothSpeed = 10f;
 	public Vector3 offset;
-	//Extra
-	private Camera mainCam;
-	public float hitBuffer = 0.5f;
-	float originalCamDis;
-	Vector3 originalCamPos;
-	Vector3 p1;
-	Vector3 p2;
 
-    private void Start()
-    {
-		//for switching between indoors and outdoors
-		mainCam = Camera.main;
-		originalCamDis = Vector3.Distance(target.transform.position, mainCam.transform.position);
-		originalCamPos = mainCam.transform.localPosition;
-		p1 = target.transform.position;
-		p2 = originalCamPos;
-	}
-
-    private void Update()
-    {
-		//CheckForCollision();
-	}
 
     void LateUpdate()
 	{
@@ -36,23 +15,4 @@ public class CameraFollow : MonoBehaviour
 		transform.position = smoothedPosition;
 		transform.LookAt(target);
 	}
-
-
-	private void CheckForCollision()
-	{
-		p1 = target.transform.position;
-		p2 = transform.TransformPoint(originalCamPos);
-		RaycastHit hit = new RaycastHit();
-		if (Physics.Linecast(p1, p2, out hit))
-		{
-			Vector3 newCamPos = transform.InverseTransformPoint(hit.point);
-			newCamPos = new Vector3(newCamPos.x, newCamPos.y - hitBuffer, newCamPos.z + hitBuffer);
-			mainCam.transform.localPosition = newCamPos;
-		}
-		else
-		{
-			mainCam.transform.localPosition = originalCamPos;
-		}
-	}
-
 }
