1f59b267342e14a6ab86d70abef52ec9ac2866ba
Kolev <v.kolev@student.tue.nl>
2021-11-01 19:55:56 +0100

    added enemy sounds

--- Assets/Scripts/Sounds/EnemySounds.cs
+++ Assets/Scripts/Sounds/EnemySounds.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemySounds : MonoBehaviour
 {
     [SerializeField] private AudioClip patrolSound;
+    [SerializeField] private AudioClip lockInSound;
+    [SerializeField] private AudioClip chaseSound;
+    [SerializeField] private AudioClip attackSound;
 
     private AudioSource audioSource;
 
@@ -17,7 +20,29 @@ public class EnemySounds : MonoBehaviour
     {
         if (!audioSource.isPlaying)
         {
+            audioSource.Stop();
             audioSource.PlayOneShot(patrolSound);
+        }
+    }
+    public void LockIn()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(lockInSound);
+        }
+    }
+    public void Chase()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(chaseSound);
+        }
+    }
+    public void Attack()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(attackSound);
         }
     }
 }
